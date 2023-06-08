eeb715fb01841682c4bf45e7e5128ed987b67da8
Sam Pursglove <spursglove@gmail.com>
2023-06-08 14:58:42 +0800

    Process hashing, documentation, new organization, ps remoting issues outstanding

--- Collect-ADDomainData.ps1
+++ Collect-ADDomainData.ps1
@@ -1,28 +1,44 @@
 <#
-Steps to enable PS Remoting via Group Policy
+.SYNOPSIS
+    Queries various datasets on systems in a domain environment.
+.DESCRIPTION
+    
+.PARAMETER OUName
+    The OU name of interest
+.EXAMPLE
+    .\Collect-ADDomainData.ps1 -OUName <ou_name>
+.NOTES
+    Version 1.0
+    Author: Sam Pursglove
+    Last modified: 08 JUN 2023
 
-Enable the WinRM service (set IPv4/IPv6 filters to all (*))
-	Computer Configuration > Policies > Administrative Templates > Windows Components > Windows Remote Management (WinRM) > WinRM Service > Allow remote server management through WinRM
+    **Steps to enable PS Remoting via Group Policy**
 
-Set the WS-Management service to automatic startup
-	Computer Configuration > Policies > Windows Settings > Security Settings > System Services > Windows Remote Management (WS-Management)
+    1) Enable the WinRM service (set IPv4/IPv6 filters to all (*))
+	    Computer Configuration > Policies > Administrative Templates > Windows Components > Windows Remote Management (WinRM) > WinRM Service > Allow remote server management through WinRM
 
-Allow Windows Remote Management in the Firewall
-	Navigate to the following folder in the Group Policy Management Console (GPMC), right-click Inbound Rules, and click New Rule.
+    2) Set the WS-Management service to automatic startup
+	    Computer Configuration > Policies > Windows Settings > Security Settings > System Services > Windows Remote Management (WS-Management)
 
-		Computer Configuration > Policies > Windows Settings > Security Settings > Windows (Defender) Firewall with Advanced Security
+    3) Allow Windows Remote Management in the Firewall
+	    Navigate to the following folder in the Group Policy Management Console (GPMC), right-click Inbound Rules, and click New Rule.
 
-		In the Predefined field, select Windows Remote Management and then follow the wizard to add the new firewall rule.
+		    Computer Configuration > Policies > Windows Settings > Security Settings > Windows (Defender) Firewall with Advanced Security
+
+		    In the Predefined field, select Windows Remote Management and then follow the wizard to add the new firewall rule.
 #>
 
-$distinguishedName = (Get-ADDomain).DistinguishedName
+param (
+    [Parameter(Position=0, Mandatory, HelpMessage='Target OU name')]
+    [string]$OUName
+)
 
 <#
 Functions
 #>
 
 # Get system TCP session and related process information
-function netConnects() {
+function netConnects {
     $hashtable = @{}
     $date = Get-Date -Format "MM/dd/yyyy"
     $time = Get-Date -Format "HH:mm"
@@ -39,7 +55,7 @@ function netConnects() {
 
 
 # Try to first get local user account info using the PS cmdlet but if that is unavailable use WMI to get the data
-function getLocalUsers() {
+function getLocalUsers {
     try {
         Get-LocalUser |
             Select-Object Name,SID,Enabled,PasswordRequired,@{Name='PasswordChangeable'; Expression={$_.UserMayChangePassword}},PrincipalSource,Description,PasswordLastSet,LastLogon
@@ -54,29 +70,31 @@ function getLocalUsers() {
 # note: attempts to use the CIM WMI cmdlets would not work in my domain environment locally or remotely and it's unknown why
 #   1) Get-CimInstance -Query "Associators of {Win32_Group.Domain='$env:COMPUTERNAME',Name='Administrators'} where Role=GroupComponent"
 #   2) Get-CimInstance -ClassName Win32_Group -Filter "Name='Administrators'" | Get-CimAssociatedInstance -Association Win32_GroupUser
-function getLocalGroupMembers() {
+function getLocalGroupMembers {
     try {
         # get all local groups
         $groups = Get-LocalGroup
 
         # get the membership for all local groups
-	    foreach ($group in $groups) {
-    	    Get-LocalGroupMember $group | 
-                ForEach-Object {
-                    [pscustomobject]@{
-                        GroupName       = $group.Name
-                        Name            = $_.Name.split('\')[1]
-                        Domain          = $_.Name.split('\')[0]
-                        SID             = $_.SID
-                        PrincipalSource = $_.PrincipalSource
-                        ObjectClass     = $_.ObjectClass
-                    } 
-                }
+	    # NOTE!!!! cannot use [pscustomobject] in remoting b/c of constrained language mode limits to core types
+        foreach ($group in $groups) {
+    	    $localGroupMem = Get-LocalGroupMember $group
+            foreach($member in $localGroupMem) {
+                [pscustomobject]@{
+                    GroupName       = $group.Name
+                    Name            = $member.Name.split('\')[1]
+                    Domain          = $member.Name.split('\')[0]
+                    SID             = $member.SID
+                    PrincipalSource = $member.PrincipalSource
+                    ObjectClass     = $member.ObjectClass
+                } 
+            }
         }
     
     # run if the Get-Local* cmdlets are not installed on the remote systems
     } catch [System.Management.Automation.RuntimeException] {
-        
+        Write-Output "In the CATCH!"
+
         # convert the provided value to a readable SID
         function ConvertTo-SID {
             Param([byte[]]$BinarySID)
@@ -88,7 +106,7 @@ function getLocalGroupMembers() {
         function localGroupMember {
             Param($Group)
             
-            $group.Invoke('members') | ForEach-Object {
+            $Group.Invoke('members') | ForEach-Object {
                 # parse the ADSPath to get the domain and determine if it's a local object or from AD
                 $_.GetType().InvokeMember("ADSPath", 'GetProperty', $null, $_, $null) -match "WinNT:\/\/(\w+)\/(.+)\/" | Out-Null
             
@@ -118,18 +136,16 @@ function getLocalGroupMembers() {
         $groups = $adsi.Children | Where-Object {$_.SchemaClassName -eq 'group'}
 
         # get group members for each local group
-        if ($groups) {
-            $groupMembers = $groups | 
-                ForEach-Object {
-                    [pscustomobject]@{
-                        Computername = $env:COMPUTERNAME
-                        GroupName    = $_.Name[0]
-                        GroupMembers = (localGroupMember -Group $_)
-                    }
-                # ignore groups with no members
-                } | Where-Object {$_.GroupMembers -notlike ''}
-        }
-
+        $groupMembers = foreach($g in $groups) { 
+            [pscustomobject]@{
+                Computername = $env:COMPUTERNAME
+                GroupName    = $g.Name[0]
+                GroupMembers = (localGroupMember -Group $g)
+            } 
+        } 
+        # ignore groups with no members
+        $groupMembers | Where-Object {$_.GroupMembers -notlike ''}
+        
         # output the combined group and individual group member data
         foreach($group in $groupMembers) {
             foreach($member in $group.GroupMembers) {
@@ -151,25 +167,28 @@ function getLocalGroupMembers() {
 <#
 Build PowerShell sessions for query reuse
 #>
+$distinguishedName = 'OU=' + $OUName + ',' + (Get-ADDomain).DistinguishedName
 
-# Pull Windows computer objects listed in the Directory
-$computers = Get-ADComputer -Filter * -SearchBase $distinguishedName -Properties OperatingSystem,LastLogonDate |
-                Where-Object {$_.OperatingSystem -like "Windows*"}
+# Pull all computer objects listed in the Directory for the designated DN
+$computers = Get-ADComputer -Filter * -Properties DistinguishedName,Enabled,IPv4Address,LastLogonDate,Name,OperatingSystem,SamAccountName -SearchBase $distinguishedName
 
-# Minimize your presence and don't create a user profile on every system (e.g., C:\Users\<username>)
-$sessionOpt = New-PSSessionOption -NoMachineProfile
+# Export domain computer account info
+$computers | Export-Csv -Path domain_computers.csv -NoTypeInformation
 
-# Create reusable PS Sessions
-$sessions = New-PSSession -ComputerName $computers.Name -SessionOption $sessionOpt
+# Create PS sessions for Windows only systems
+$computers = $computers | Where-Object {$_.OperatingSystem -like "Windows*"}
+$sessionOpt = New-PSSessionOption -NoMachineProfile # Minimize your presence and don't create a user profile on every system (e.g., C:\Users\<username>)
+$sessions = New-PSSession -ComputerName $computers.Name -SessionOption $sessionOpt # Create reusable PS Sessions
 
 
-
+<#
+# Attempt to enable WinRM/PS remoting via WMI for systems that don't have it configured
 foreach($computer in $failedSessions) {
     $cimSessOption = New-CimSessionOption -Protocol Dcom
     $cimSession = New-CimSession -ComputerName <name> -SessionOption $cimSessOption
     Invoke-CimMethod -ClassName 'Win32_Process' -MethodName 'Create' -CimSession $cimSession -Arguments @{CommandLine = "powershell Start-Process powershell -ArgumentList 'Enable-PSRemoting -Force'"}
 }
-
+#>
 
 
 <#
@@ -177,15 +196,18 @@ Pull remote system data
 #>
 
 # Local Administrators group membership
+# !!!! Doesn't work remotely (see comments in the fuction above
+<#
 Invoke-Command -Session $sessions -ScriptBlock ${function:getLocalGroupMembers} |
     Export-Csv -Path local_admins_group.csv -NoTypeInformation
+#>
 
 # Local user accounts
 Invoke-Command -Session $sessions -ScriptBlock ${function:getLocalUsers} | 
 	Export-Csv -Path local_users.csv -NoTypeInformation
 
 # Processes
-Invoke-Command -Session $sessions -ScriptBlock {Get-Process -IncludeUserName | Select-Object Name,Id,Path,UserName,Company,Description,ProductVersion,StartTime} |
+Invoke-Command -Session $sessions -ScriptBlock {Get-Process -IncludeUserName | Select-Object Name,Id,Path,@{Name='Hash'; Expression={if($_.Path -notlike '') {(Get-FileHash $_.Path).Hash}}},UserName,Company,Description,ProductVersion,StartTime} |
 	Export-Csv -Path processes.csv -NoTypeInformation
 
 # Scheduled tasks
@@ -206,7 +228,7 @@ Invoke-Command -Session $sessions -ScriptBlock {Get-ChildItem -Path 'C:\Program 
 
 # 32 bit programs
 Invoke-Command -Session $sessions -ScriptBlock {Get-ChildItem -Path 'C:\Program Files (x86)' | Select-Object Name,CreationTime,LastAccessTime,LastWriteTime,Attributes,@{Name='ProgramType'; Expression={'32-bit'}}} |
-	Export-Csv -Path programs.csv -NoTypeInformation
+	Export-Csv -Path programs.csv -Append -NoTypeInformation
 
 # Network connections
 Invoke-Command -Session $sessions -ScriptBlock ${function:netConnects} |
@@ -216,60 +238,61 @@ Remove-PSSession -Session $sessions
 
 
 <#
-Pull data from the local system and append to the existing CSV files
+    Pull data from the local system and append to the existing CSV files
+    Note: I put in a function so it won't call the code automatically - still needs work
 #>
+function Collect-LocalSystemData {
+    # Local Administrators group membership
+    getLocalGroupMembers |
+	    Export-Csv -Path local_admins_group.csv -Append -NoTypeInformation
 
-# Local Administrators group membership
-getlocalGroupMembers |
-	Export-Csv -Path local_admins_group.csv -Append -NoTypeInformation
+    # Local user accounts
+    getLocalUsers |
+	    Export-Csv -Path local_users.csv -Append -NoTypeInformation
 
-# Local user accounts
-getLocalUsers |
-	Export-Csv -Path local_users.csv -Append -NoTypeInformation
+    # Processes
+    # Check if the local session is running with elevated privileges
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $localProcesses = Get-Process -IncludeUserName
+    } else {
+        $localProcesses = Get-Process
+    }
 
-# Processes
-# Check if the local session is running with elevated privileges
-$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$p = New-Object System.Security.Principal.WindowsPrincipal($id)
-if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $localProcesses = Get-Process -IncludeUserName
-} else {
-    $localProcesses = Get-Process
+    $localProcesses |
+        Select-Object Name,Id,Path,@{Name='Hash'; Expression={if($_.Path -notlike '') {(Get-FileHash $_.Path).Hash}}},UserName,Company,Description,ProductVersion,StartTime |
+	    Export-Csv -Path processes.csv -Append -NoTypeInformation
+
+    # Scheduled tasks
+    Get-ScheduledTask |
+        Select-Object TaskName,State,Author,TaskPath,Description |
+	    Export-Csv -Path scheduled_tasks.csv -Append -NoTypeInformation
+
+    # Services
+    Get-Service |
+        Select-Object Name,DisplayName,Status,StartType,ServiceType |
+	    Export-Csv -Path services.csv -Append -NoTypeInformation
+
+    # Downloads, Documents, and Desktop files
+    Get-ChildItem -Path 'C:\Users\*\Downloads\','C:\Users\*\Documents\','C:\Users\*\Desktop\' -Recurse |
+        Select-Object Name,Extension,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes |
+	    Export-Csv -Path files.csv -Append -NoTypeInformation
+
+    # 64 bit programs
+    Get-ChildItem -Path 'C:\Program Files' |
+        Select-Object Name,CreationTime,LastAccessTime,LastWriteTime,Attributes,@{Name='ProgramType'; Expression={'64-bit'}} |
+	    Export-Csv -Path programs.csv -Append -NoTypeInformation
+
+    # 32 bit programs
+    Get-ChildItem -Path 'C:\Program Files (x86)' |
+        Select-Object Name,CreationTime,LastAccessTime,LastWriteTime,Attributes,@{Name='ProgramType'; Expression={'32-bit'}} |
+	    Export-Csv -Path programs.csv -Append -NoTypeInformation
+
+    # Network connections
+    netConnects |
+        Export-Csv -Path net.csv -Append -NoTypeInformation
 }
-
-$localProcesses |
-    Select-Object Name,Id,Path,UserName,Company,Description,ProductVersion,StartTime |
-	Export-Csv -Path processes.csv -Append -NoTypeInformation
-
-# Scheduled tasks
-Get-ScheduledTask |
-    Select-Object TaskName,State,Author,TaskPath,Description |
-	Export-Csv -Path scheduled_tasks.csv -Append -NoTypeInformation
-
-# Services
-Get-Service |
-    Select-Object Name,DisplayName,Status,StartType,ServiceType |
-	Export-Csv -Path services.csv -Append -NoTypeInformation
-
-# Downloads, Documents, and Desktop files
-Get-ChildItem -Path 'C:\Users\*\Downloads\','C:\Users\*\Documents\','C:\Users\*\Desktop\' -Recurse |
-    Select-Object Name,Extension,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes |
-	Export-Csv -Path files.csv -Append -NoTypeInformation
-
-# 64 bit programs
-Get-ChildItem -Path 'C:\Program Files' |
-    Select-Object Name,CreationTime,LastAccessTime,LastWriteTime,Attributes,@{Name='ProgramType'; Expression={'64-bit'}} |
-	Export-Csv -Path programs.csv -Append -NoTypeInformation
-
-# 32 bit programs
-Get-ChildItem -Path 'C:\Program Files (x86)' |
-    Select-Object Name,CreationTime,LastAccessTime,LastWriteTime,Attributes,@{Name='ProgramType'; Expression={'32-bit'}} |
-	Export-Csv -Path programs.csv -Append -NoTypeInformation
-
-# Network connections
-netConnects |
-    Export-Csv -Path net.csv -Append -NoTypeInformation
-
 
 <#
 Pull Active Directory datasets
@@ -278,10 +301,6 @@ Pull Active Directory datasets
 # Get domain user account information
 Get-ADUser -Filter * -Properties AccountExpirationDate,AccountNotDelegated,AllowReversiblePasswordEncryption,CannotChangePassword,DisplayName,Name,Enabled,LastLogonDate,LockedOut,PasswordExpired,PasswordNeverExpires,PasswordNotRequired,SamAccountName,SmartcardLogonRequired -SearchBase $distinguishedName |
 	Export-Csv -Path domain_users.csv -NoTypeInformation
-
-# Get domain computer account info
-Get-ADComputer -Filter * -Properties DistinguishedName,Enabled,IPv4Address,LastLogonDate,Name,OperatingSystem,SamAccountName -SearchBase $distinguishedName |
-	Export-Csv -Path domain_computers.csv -NoTypeInformation
 
 # Get privileged domain account group memberships
 $adminMemberOf = New-Object System.Collections.ArrayList
